49cf33f590c6f66fa40e7de590a4446c4dbe3466
Nana Quam <quamgyambrah@gmail.com>
2025-02-02 08:18:54 -0500

    added fetching movies from api functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,91 @@
-import React, { useState } from 'react'
-import Search from './components/Search'
+import React, { useEffect, useState } from "react";
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const API_BASE_URL = "https://api.themoviedb.org/3";
+  const ACCESS_TOKEN = import.meta.env.VITE_TMBD_ACCESS_TOKEN;
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  };
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.Response == "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.log(`Error occurred while fetching movies ${error}`);
+      setErrorMessage(
+        "Error occurred while fetching movies. Please try again later."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
       <div className="pattern" />
-        <div className='wrapper'>
-          <header>
+      <div className="wrapper">
+        <header>
           <img src="./hero-img.png" alt="hero-img" />
-          <h1>Escape into a world of endless <span className='text-gradient'>entertainment</span>  🎬🍿</h1>
+          <h1>
+            Escape into a world of endless{" "}
+            <span className="text-gradient">entertainment</span> 🎬🍿
+          </h1>
 
-          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <div className="text-3xl text-white">{searchTerm}</div>
 
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
 
-          <div className='text-3xl text-white'>{searchTerm}</div>
-        </div>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p className="text-white">{movie.title}</p>
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
